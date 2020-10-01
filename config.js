5172e568bceff7166ef3339760355a68559c4dd4
Martin <martin.r.matousek@gmail.com>
2020-10-01 16:55:01 +0200

    Update config.js
    Moved Mongo connection string to environment variables

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const JWTSECRET = process.env.JWTSECRET;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = process.env.DB_URL;
 
 module.exports = {
     jwtSecret: JWTSECRET,
-    mongodburi: 'mongodb://' + DB_USERNAME + ':' + DB_PASSWORD + '@ds233763.mlab.com:33763/basic-mern-stack-app'
+    mongodburi: DB_URL
 };
