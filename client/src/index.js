daabb9ba953195b6e3074f7815c5152930f7b547
Mansi Mishra <mansimishra510@gmail.com>
2021-10-30 20:26:48 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -26,6 +26,23 @@ const app = (
     </Provider>
 );
 
+const chk = document.getElementById('chk');
 
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
 ReactDOM.render(app, document.getElementById('root'));
 // registerServiceWorker();
